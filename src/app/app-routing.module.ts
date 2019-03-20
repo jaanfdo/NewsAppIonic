dbc20b929daf4e66e4ce6e0a0237a1806d9c2303
Jaan <janithsrimal@gmail.com>
2019-03-20 14:59:04 +0530

    add news 2019-03-20

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'news', loadChildren: './news/news.module#NewsPageModule' },
+  { path: 'news-one', loadChildren: './news-one/news-one.module#NewsOnePageModule' }
 ];
 @NgModule({
   imports: [
---
+++ src/app/news-one/news-one.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news-one',
+  templateUrl: './news-one.page.html',
+  styleUrls: ['./news-one.page.scss'],
+})
+export class NewsOnePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
